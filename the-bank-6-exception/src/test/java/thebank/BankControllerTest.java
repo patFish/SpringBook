f4eae09d39d073d0206b2c31a8145936229f136e
p.fischer <p.fischer@accenture.com>
2016-09-29 15:18:41 +0200

    added working unit case

--- the-bank-6-exception/src/test/java/thebank/BankControllerTest.java
+++ the-bank-6-exception/src/test/java/thebank/BankControllerTest.java
@@ -6,20 +6,18 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
-import java.io.IOException;
-
 import org.json.JSONObject;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.mock.http.MockHttpOutputMessage;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(BankController.class)
@@ -34,8 +32,6 @@ public class BankControllerTest {
 
 	@MockBean
 	BankService bankService;
-
-	private HttpMessageConverter<Object> mappingJackson2HttpMessageConverter;
 
 	private SavingAccount standardSavingAccountForTest;
 	// @Autowired
@@ -73,23 +69,11 @@ public class BankControllerTest {
 		standardSavingAccountForTest.setId(1);
 		standardSavingAccountForTest.setBalance(1000);
 		JSONObject jsonc = new JSONObject(standardSavingAccountForTest);
-		this.mockMvc
+		MvcResult result = this.mockMvc
 				.perform(post("/bank/saveAccount").contentType(MediaType.APPLICATION_JSON).content(jsonc.toString()))
-				.andExpect(content().string(jsonc.toString()));
-
-		// Account entity = (Account)
-		// this.testRestTemplate.getForEntity("http://localhost:8080/saveAccount",
-		// Account.class);
-
-		// this.mockMvc.perform(get("/saveAccount").accept(MediaType.TEXT_PLAIN)).andExpect(status().isOk())
-		// .andExpect(content().string("Honda Civic"));
-		// assertEquals(temp, testReturnValue);
-	}
-
-	protected String json(Object o) throws IOException {
-		MockHttpOutputMessage mockHttpOutputMessage = new MockHttpOutputMessage();
-		this.mappingJackson2HttpMessageConverter.write(o, MediaType.APPLICATION_JSON, mockHttpOutputMessage);
-		return mockHttpOutputMessage.getBodyAsString();
+				.andReturn();
+		String resultAsString = result.getResponse().getContentAsString();
+		JSONAssert.assertEquals(resultAsString, jsonc, false);
 	}
 
 }
